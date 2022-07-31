7781882a2f569385c74093e4d40ddf67f9ebaf8d
RinkM <mikerinkert@gmail.com>
2022-07-31 11:20:47 -0600

    fixed PW confirm error

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const lastNameError = document.getElementById("lastNameError")
 const emailError = document.getElementById("emailError")
 const phoneError = document.getElementById("phoneError")
 const passwordError = document.getElementById("passwordError")
-const passwordConfirmError = document.getElementById("passwordConfirm")
+const passwordConfirmError = document.getElementById("passwordConfirmError")
 
 const phoneReg = /[0-9]{3}-[0-9]{3}-[0-9]{4}/
 const emailReg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
@@ -16,18 +16,22 @@ const form = document.getElementById('signUpForm');
 let userInfo = {}
 
 const verify = () => {
-  if (userInfo.password !== userInfo.passwordConfirm){
-    passwordError.style.display = "block"
-  }
+
   if (phoneReg.test(userInfo.phone)==false){
-    phoneError.style.display = "block"
+    phoneError.style.display = "block";
+    return false
+  }
+  if (userInfo.password !== userInfo.passwordConfirm){
+    passwordConfirmError.style.display = "block";
+    return false
   }
   if (emailReg.test(userInfo.email)==false){
-    emailError.style.display = "block"
+    emailError.style.display = "block";
+    return false
   }
+  return true
 
 }
-
 
 
 form.addEventListener('submit', function(e) {
@@ -37,7 +41,7 @@ form.addEventListener('submit', function(e) {
   for (var input of entries) {
     (userInfo[input[0]] = input[1]);
   }
-  verify()
+  if (verify()){alert("Thanks for your interest! We will contact you soon.")}
   });
 
 
